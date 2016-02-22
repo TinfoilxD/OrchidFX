1933712dd5fa0eb42450b782d0348ac263f37706
Geralyn <geralyn.urquhart@gmail.com>
2016-02-22 10:20:38 -0600

    working connection

--- src/swingproject/DatabaseConnectionTest.java
+++ src/swingproject/DatabaseConnectionTest.java
@@ -8,22 +8,23 @@ public class DatabaseConnectionTest
 {
 
 
-    private final String driverType = "jdbc";
-    private final String dbmsType = "sqlserver";
-    private final String hostname = "//192.168.1.66";
-    private final String portNumber = "1433";
-    private final String databaseName = "TestDB1";
-    private final String connectionString = String.format("%s:%s:%s:%s", driverType, dbmsType, hostname, portNumber);
-    private Connection databaseConnection;
+    public final String driverType = "jdbc";
+    public final String dbmsType = "sqlserver";
+    public final String hostname = "//192.168.1.66"; //192.168.1.75  192.168.1.66
+    public final String portNumber = "1433";
+    public final String databaseName = "databaseName=TestDB1"; //ANDREWSURFACE TestDB1
+    public final String propertyValue = "user=Geralyn;password=testtest"; // integratedSecurity=true;   user=Geralyn;password=testtest
+    public final String connectionString = String.format("%s:%s:%s:%s;%s;%s", driverType, dbmsType, hostname, portNumber, databaseName, propertyValue);
+    public Connection databaseConnection;
+
 
 
     public DatabaseConnectionTest() throws ClassNotFoundException, SQLException
     {
+        System.out.println(connectionString);
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         databaseConnection = DriverManager.getConnection(connectionString);
         System.out.println(databaseConnection.isValid(30));
-
-
     }
 
 
@@ -31,11 +32,13 @@ public class DatabaseConnectionTest
     {
         try
         {
+
             DatabaseConnectionTest databaseConnectionTest = new DatabaseConnectionTest();
         }
         catch(Exception e)
         {
-        System.out.println(e);
+            System.out.println(e);
+
         }
 
 
