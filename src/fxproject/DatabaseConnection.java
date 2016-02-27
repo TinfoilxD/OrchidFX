57d0901d52c2ac4fd98aa65e0b8d4c3fc14085d1
TinfoilxD <TinfoilxD@gmail.com>
2016-02-27 15:10:12 -0600

    Fixed Merge errors

--- src/fxproject/DatabaseConnection.java
+++ src/fxproject/DatabaseConnection.java
@@ -10,17 +10,17 @@ public class DatabaseConnection
     private final String dbmsType = "sqlserver";
     private final String hostname = "//192.168.1.66"; //192.168.1.75  192.168.1.66
     private final int portNumber = 1433;
-    private final String databaseName = "databaseName=TestDB1"; //ANDREWSURFACE TestDB1
+    private final String databaseName = "databaseName=OrchidDB"; //ANDREWSURFACE TestDB1
     private final String propertyValue = "user=Geralyn;password=testtest"; // integratedSecurity=true;   user=Geralyn;password=testtest
-    private final String userName = "Geralyn";
-    private final String password = "testtest";
+    private final String userName = "Tin";
+    private final String password = "test";
     private final String connectionString = String.format("%s:%s:%s:%s;%s;%s", driverType, dbmsType, hostname, portNumber, databaseName, propertyValue);
 
 
     private SQLServerDataSource dataSource;
-    private Connection databaseConnection;
-    private CallableStatement callableStatement;
-    private ResultSet resultSet;
+    private Connection connection;
+
+    private static DatabaseConnection databaseConnection;
 
     public DatabaseConnection()
     {
@@ -40,13 +40,13 @@ public class DatabaseConnection
     public Connection getSingleConnection() throws ClassNotFoundException, SQLException
     {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        databaseConnection = DriverManager.getConnection(connectionString);
-        return databaseConnection;
+        connection = DriverManager.getConnection(connectionString);
+        return connection;
     }
     public Connection getConnection() throws SQLException
     {
-        databaseConnection = dataSource.getConnection();
-        return databaseConnection;
+        connection = dataSource.getConnection();
+        return connection;
     }
 
 }
