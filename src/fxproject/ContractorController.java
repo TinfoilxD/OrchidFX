26c9bdf53d3a288772c5999b2a5ebcb89f6c2777
Geralyn <geralyn.urquhart@gmail.com>
2016-03-21 14:01:43 -0500

    Added Country and State Controllers, etc.

--- src/fxproject/ContractorController.java
+++ src/fxproject/ContractorController.java
@@ -4,6 +4,7 @@ package fxproject;/*
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,7 +13,10 @@ import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import orchidmodel.CountryModel;
+import orchidmodel.StateModel;
 
+
+import java.awt.*;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -20,9 +24,11 @@ import java.util.ResourceBundle;
 
 public class ContractorController
 {
+
     public static final String VIEWCONTROLLER_TITLE = "Contractor Input";
     @FXML
     ComboBox<String> fxSelectCountry;
+    ObservableList<CountryModel> countryList;
     public ContractorController()
     {
 
@@ -60,12 +66,14 @@ public class ContractorController
     {
        setFxSelectCountry();
     }
+
+
     public void setFxSelectCountry()
     {
         try
         {
-            ObservableList<CountryModel> countryList = new CountryProcedureSet().procSelectCountries();
-            ObservableList<String> countryNameList = FXCollections.observableArrayList();
+            countryList = new CountryProcedureSet().procSelectCountries(); //countryList has a list of all the Country Models
+            ObservableList<String> countryNameList = FXCollections.observableArrayList(); //countryNameList has the list of string Country Names
             int defaultIndex = 0;
             for(int i = 0; i < countryList.size(); i++)
             {
@@ -85,5 +93,48 @@ public class ContractorController
 
     }
 
+    @FXML
+    private void handleComboBoxCountryAction(ActionEvent e)
+    {
+        setFxSelectState();
+    }
+
+    @FXML
+    public void setFxSelectState()
+    {
+        //get selected country id
+        //pass id into procedure
+        //get list from procedure
+        //create list of just state names
+        //set combobox list
+
+
+        try
+        {
+            int countryIdx = fxSelectCountry.getSelectionModel().getSelectedIndex();
+            int countryId = countryList.get(countryIdx).getCountryID();
+            ObservableList<StateModel> stateList = new StateProcedureSet().procSelectState(countryId);
+
+
+
+            ObservableList<String> countryNameList = FXCollections.observableArrayList();
+            int defaultIndex = 0;
+            for(int i = 0; i < countryList.size(); i++)
+            {
+                CountryModel m = countryList.get(i);
+                String countryAbbreviation = m.getCountryAbbreviation();
+                if(countryAbbreviation.equals("USA"))
+                    defaultIndex = i;
+                countryNameList.add(m.getCountryName());
+            }
+            fxSelectCountry.setItems(countryNameList);
+            fxSelectCountry.getSelectionModel().select(defaultIndex);
+        }
+        catch(Exception e)
+        {
+            System.out.println("An error has occured that doesn't actually do anything.");
+        }
+
+    }
 
 }
