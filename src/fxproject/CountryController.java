28458e062f6a2720557624711f39706c607ca420
Andrew <astiefler@hotmail.com>
2016-04-12 20:15:58 -0500

    Andrew Country commit

--- src/fxproject/CountryController.java
+++ src/fxproject/CountryController.java
@@ -6,6 +6,8 @@ package fxproject;/*
 import com.microsoft.sqlserver.jdbc.SQLServerResultSet;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -14,6 +16,10 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import orchidmodel.CountryModel;
 
 public class CountryController
@@ -23,9 +29,34 @@ public class CountryController
 
     public static final String VIEWCONTROLLER_TITLE= "Country Input";
 
+
+    @FXML
+    TextField abbreviationtextfield;
+
+    @FXML
+    Button deletecountrybutton;
+
+    @FXML
+    Button cancelcountrybutton;
+
+    @FXML
+    Button newupdatecountrybutton;
+
+    @FXML
+    ComboBox comboboxcountryname;
+
+
+
     public CountryController()
     {
 
+    }
+
+    @FXML
+    public void initialize()
+    {
+        comboboxcountryname.setEditable(true);
+        setFxSelectCountry();
     }
 
     public NodeBundle loadBundle()
@@ -46,5 +77,58 @@ public class CountryController
         }
         return null;
     }
+
+    public void setFxSelectCountry()
+    {
+        try
+        {
+            ObservableList<CountryModel> countryList = new CountryProcedureSet().procSelectCountries(); //countryList has a list of all the Country Models
+            ObservableList<String> countryNameList = FXCollections.observableArrayList(); //countryNameList has the list of string Country Names
+            int defaultIndex = 0;
+            for(int i = 0; i < countryList.size(); i++)
+            {
+                CountryModel m = countryList.get(i);
+                String countryAbbreviation = m.getCountryAbbreviation();
+                if(countryAbbreviation.equals("USA"))
+                    defaultIndex = i;
+                countryNameList.add(m.getCountryName());
+            }
+
+            comboboxcountryname.setItems(countryNameList);
+            comboboxcountryname.getSelectionModel().select(defaultIndex);
+        }
+        catch(Exception e)
+        {
+            //System.out.println("An error has occured that doesn't actually do anything.");
+        }
+
+    }
+
+    @FXML
+    private void handleComboBoxCountryAction(ActionEvent e) {
+        fillFXAbbreviation();
+    }
+
+
+
+    public void fillFXAbbreviation()
+    {
+        try {
+
+
+
+
+
+
+
+
+        }
+        catch(Exception e2)
+        {
+
+        }
+    }
+
+
 
 }
