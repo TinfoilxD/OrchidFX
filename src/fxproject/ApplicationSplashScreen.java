2b2b36120b9c3186b71b5c69c964f4b73c43dd63
Geralyn <geralyn.urquhart@gmail.com>
2016-04-15 20:20:50 -0500

    Fixed all the things

--- src/fxproject/ApplicationSplashScreen.java
+++ src/fxproject/ApplicationSplashScreen.java
@@ -1,93 +1,59 @@
-package fxproject;/*
- * Written by Tin Van on 2/15/16.
- */
+package fxproject;
 
-import javafx.animation.PauseTransition;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-import javafx.util.Duration;
-
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 
 public class ApplicationSplashScreen extends Application
 {
-
-    /*
-
-
-     */
-
     Stage window;
     public static void main(String args[])
     {
+
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         window = primaryStage;
-        window.setScene(loadView());
-        window.initStyle(StageStyle.UNDECORATED);
-        window.sizeToScene();
-        window.show();
-
-        PauseTransition pause = new PauseTransition(Duration.millis(500));
-        pause.setOnFinished(e -> {
-            loadDatabaseScreen();
-            window.close();
-        });
-        pause.playFromStart();
-
-
-
-
-
+        loadDatabaseScreen();
+         window.close();
     }
-    private Scene loadView()
-    {
-        try
-        {
-            //FXMLLoader loader = new FXMLLoader();
-            URL url = getClass().getResource("/main/resources/splashscreen.fxml");
-            Parent root = FXMLLoader.load(url);
-            Scene scene = new Scene(root);
-            return scene;
-        }
-        catch(Exception e)
-        {
-            /*
-            Error messages. This will printout to an alertbox later on during validation.
-             */
-            e.printStackTrace();
 
-        }
-        return null;
-    }
-    private void loadDatabaseScreen()
-    {
+    private void loadDatabaseScreen() throws IOException {
         try
         {
             Stage stage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("../main/resources/DatabaseSettingsForm.fxml"));
+
+            Parent root = FXMLLoader.load(getClass().getResource("/DatabaseSettingsForm.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.sizeToScene();
             stage.show();
+
+//            Stage stage = new Stage();
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/DatabaseSettingsForm.fxml"));
+//           // loader.setController(new DataSettingsController());
+//            Parent root = loader.load();
+//            Scene scene = new Scene(root);
+//            stage.setScene(scene);
+//            stage.sizeToScene();
+//            stage.show();
         }
         catch(Exception e)
         {
+            FileWriter fw = new FileWriter ("G:\\Orchid\\run.log", true);
+            PrintWriter pw = new PrintWriter (fw);
+            e.printStackTrace (pw);
+            pw.close();
             new OrchidAlertBox("Error",e.toString());
         }
     }
-
-
-
 }
