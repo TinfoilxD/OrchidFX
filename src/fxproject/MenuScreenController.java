04e85cf2a60d9963d9bee2c5236a53d42bdc9374
TinfoilxD <TinfoilxD@gmail.com>
2016-04-19 17:42:10 -0500

    ProjectController in progress.

--- src/fxproject/MenuScreenController.java
+++ src/fxproject/MenuScreenController.java
@@ -56,7 +56,7 @@ public class MenuScreenController
     @FXML
     protected void handleProjectNewProjectAction(ActionEvent event)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ProjectController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewProjectForm.fxml");
     }
 
     @FXML
