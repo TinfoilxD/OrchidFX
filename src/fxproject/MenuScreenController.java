99dd11e0bfa6e1f73fff75a6f94c74a04a4c7a7e
Geralyn <geralyn.urquhart@gmail.com>
2016-04-19 17:42:50 -0500

    Connected forms

--- src/fxproject/MenuScreenController.java
+++ src/fxproject/MenuScreenController.java
@@ -98,158 +98,385 @@ public class MenuScreenController
         reportAction("Hotel By Status", "{call SelectHotelByStatus}");
     }
 
-
-
-    /*
-    DEVELOPER TAB ACTIONS GO HERE
-     */
+    //    /*
+//    DEVELOPER  TAB ACTIONS GO HERE
+//     */
 
     @FXML
-    protected void handleDeveloperClientNewAction(ActionEvent event)
+    protected void developerForm1(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ClientController.VIEWCONTROLLER_TITLE);
+        openChildForm("AssociativeTables.fxml");
     }
     @FXML
-    protected void handleDeveloperClientContactNewClientContactAction(ActionEvent event)
+    protected void developerForm2(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ClientContactController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewClientChildForm.fxml");
     }
     @FXML
-    protected void handleDeveloperClientContactTypeNewClientContactTypeAction(ActionEvent event)
+    protected void developerForm3(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ClientContactTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("SearchClientForm.fxml");
     }
     @FXML
-    protected void handleDeveloperClientStatusNewClientStatusAction(ActionEvent event)
+    protected void developerForm4(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ClientStatusController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewClientStatusForm.fxml");
     }
     @FXML
-    protected void handleDeveloperCMVStatusNewClientStatusAction(ActionEvent event)
+    protected void developerForm5(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(CMVStatusController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewClientContactForm.fxml");
     }
     @FXML
-    protected void handleDeveloperContractorNewContractorAction(ActionEvent event)
+    protected void developerForm6(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewClientContactTypeForm.fxml");
     }
     @FXML
-    protected void handleDeveloperContractorContactNewContractorContactAction(ActionEvent event)
+    protected void developerForm7(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorContactController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewClientContactContactTypeForm.fxml");
     }
     @FXML
-    protected void handleDeveloperContractorContactTypeNewContractorContactTypeAction(ActionEvent event)
+    protected void developerForm8(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorContactTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewCMVStatusForm.fxml");
     }
     @FXML
-    protected void handleDeveloperDepartmentNewDepartmentAction(ActionEvent event)
+    protected void developerForm9(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(DepartmentController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewContractorForm.fxml");
     }
     @FXML
-    protected void handleDeveloperHotelPropertyNewHotelPropertyAction(ActionEvent event)
+    protected void developerForm10(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(HotelPropertyController.VIEWCONTROLLER_TITLE);
+        openChildForm("ContractorContactForm.fxml");
     }
     @FXML
-    protected void handleDeveloperManufacturerNewManufacturerAction(ActionEvent event)
+    protected void developerForm11(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewContractorContactTypeForm.fxml");
     }
     @FXML
-    protected void handleDeveloperManufacturerContactNewManufacturerContactAction(ActionEvent event)
+    protected void developerForm12(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerContactController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewCountryForm.fxml");
     }
     @FXML
-    protected void handleDeveloperManufacturerContactTypeNewManufacturerTypeContactAction(ActionEvent event)
+    protected void developerForm13(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerContactTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewDepartmentForm.fxml");
     }
     @FXML
-    protected void handleDeveloperOfficeLocationNewOfficeLocationTypeAction(ActionEvent event)
+    protected void developerForm14(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(OfficeLocationController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewEmployeeForm.fxml");
     }
     @FXML
-    protected void handleDeveloperParentBrandNewParentBrandTypeAction(ActionEvent event)
+    protected void developerForm15(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ParentBrandController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewEmployeeHistory.fxml");
     }
     @FXML
-    protected void handleDeveloperPhaseLookupNewPhaseLookupTypeAction(ActionEvent event)
+    protected void developerForm16(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(PhaseLookupController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewEmployeeStatusForm.fxml");
     }
     @FXML
-    protected void handleDeveloperPhaseStatusNewPhaseStatusTypeAction(ActionEvent event)
+    protected void developerForm17(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(PhaseStatusController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewHotelApprover.fxml");
     }
     @FXML
-    protected void handleDeveloperProjectStatusNewProjectStatusAction(ActionEvent event)
+    protected void developerForm18(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ProjectStatusController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewHotelApproverTypeForm.fxml");
     }
     @FXML
-    protected void handleDeveloperProjectTypeNewProjectTypeAction(ActionEvent event)
+    protected void developerForm19(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ProjectTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewHotelBrandForm.fxml");
     }
     @FXML
-    protected void handleDeveloperReferrerNewReferrerAction(ActionEvent event)
+    protected void developerForm20(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ReferrerController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewHotelPropertyForm.fxml");
     }
     @FXML
-    protected void handleDeveloperReferrerTypeNewReferrerTypeAction(ActionEvent event)
+    protected void developerForm21(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(ReferrerTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewHotelStatusForm.fxml");
     }
     @FXML
-    protected void handleDeveloperStateNewStateAction(ActionEvent event)
+    protected void developerForm22(ActionEvent e)
+    {
+        openChildForm("NewManufacturerForm.fxml");
+    }
+    @FXML
+    protected void developerForm23(ActionEvent e)
+    {
+        openChildForm("NewManufacturerContact.fxml");
+    }
+    @FXML
+    protected void developerForm24(ActionEvent e)
+    {
+        openChildForm("NewManufacturerContactTypeForm.fxml");
+    }
+    @FXML
+    protected void developerForm25(ActionEvent e)
+    {
+        openChildForm("NewOfficeLocationForm.fxml");
+    }
+    @FXML
+    protected void developerForm26(ActionEvent e)
+    {
+        openChildForm("NewParentBrandForm.fxml");
+    }
+    @FXML
+    protected void developerForm27(ActionEvent e)
+    {
+        openChildForm("NewPhaseLookupForm.fxml");
+    }
+    @FXML
+    protected void developerForm28(ActionEvent e)
+    {
+        openChildForm("NewPhaseStatusForm.fxml");
+    }
+    @FXML
+    protected void developerForm29(ActionEvent e)
+    {
+        openChildForm("NewPhaseTaskStatusForm.fxml");
+    }
+    @FXML
+    protected void developerForm30(ActionEvent e)
+    {
+        openChildForm("NewProjectForm.fxml");
+    }
+    @FXML
+    protected void developerForm31(ActionEvent e)
+    {
+        openChildForm("NewProjectAdjustment.fxml");
+    }
+    @FXML
+    protected void developerForm32(ActionEvent e)
+    {
+        openChildForm("NewProjectStatusForm.fxml");
+    }
+    @FXML
+    protected void developerForm33(ActionEvent e)
+    {
+        openChildForm("NewProjectTypeForm.fxml");
+    }
+    @FXML
+    protected void developerForm34(ActionEvent e)
+    {
+        openChildForm("NewReferrerForm.fxml");
+    }
+    @FXML
+    protected void developerForm35(ActionEvent e)
+    {
+        openChildForm("NewReferrerTypeForm.fxml");
+    }
+    @FXML
+    protected void developerForm36(ActionEvent e)
     {
         openChildForm("NewStateForm.fxml");
     }
     @FXML
-    protected void handleDeveloperTaskListNewTaskListAction(ActionEvent event)
+    protected void developerForm37(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(TaskListController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewSubTaskListForm.fxml");
     }
     @FXML
-    protected void handleDeveloperTradeShowNewTradeShowAction(ActionEvent event)
+    protected void developerForm38(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(TradeShowController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewTaskListForm.fxml");
     }
     @FXML
-    protected void handleAdminUserNewUserAction(ActionEvent event)
+    protected void developerForm39(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(UserController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewTaskStatusForm.fxml");
     }
     @FXML
-    protected void handleDeveloperVendorNewVendorAction(ActionEvent event)
+    protected void developerForm40(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(VendorController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewTradeShowForm.fxml");
     }
     @FXML
-    protected void handleDeveloperVendorContactNewVendorContactAction(ActionEvent event)
+    protected void developerForm41(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(VendorContactController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewUserForm.fxml");
     }
     @FXML
-    protected void handleDeveloperVendorContactTypeNewVendorContactTypeAction(ActionEvent event)
+    protected void developerForm42(ActionEvent e)
     {
-        RootStageContainer.getCurrentRootStageContainer().changeView(VendorContactTypeController.VIEWCONTROLLER_TITLE);
+        openChildForm("NewVendorForm.fxml");
     }
     @FXML
-    protected void handleDeveloperCountryEditAction(ActionEvent event)
+    protected void developerForm43(ActionEvent e)
     {
+        openChildForm("NewVendorContactForm.fxml");
+    }
+    @FXML
+    protected void developerForm44(ActionEvent e)
+    {
+        openChildForm("NewVendorContactTypeForm.fxml");
+    }
 
-        openChildForm("NewCountryForm.fxml");
-    }
+
+
+
+//
+//    /*
+//    DEVELOPER2  TAB ACTIONS GO HERE
+//     */
+//
+//    @FXML
+//    protected void handleDeveloperClientNewAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ClientController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperClientContactNewClientContactAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ClientContactController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperClientContactTypeNewClientContactTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ClientContactTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperClientStatusNewClientStatusAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ClientStatusController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperCMVStatusNewClientStatusAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(CMVStatusController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperContractorNewContractorAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperContractorContactNewContractorContactAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorContactController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperContractorContactTypeNewContractorContactTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ContractorContactTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperDepartmentNewDepartmentAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(DepartmentController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperHotelPropertyNewHotelPropertyAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(HotelPropertyController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperManufacturerNewManufacturerAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperManufacturerContactNewManufacturerContactAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerContactController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperManufacturerContactTypeNewManufacturerTypeContactAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ManufacturerContactTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperOfficeLocationNewOfficeLocationTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(OfficeLocationController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperParentBrandNewParentBrandTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ParentBrandController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperPhaseLookupNewPhaseLookupTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(PhaseLookupController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperPhaseStatusNewPhaseStatusTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(PhaseStatusController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperProjectStatusNewProjectStatusAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ProjectStatusController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperProjectTypeNewProjectTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ProjectTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperReferrerNewReferrerAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ReferrerController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperReferrerTypeNewReferrerTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(ReferrerTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperStateNewStateAction(ActionEvent event)
+//    {
+//        openChildForm("NewStateForm.fxml");
+//    }
+//    @FXML
+//    protected void handleDeveloperTaskListNewTaskListAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(TaskListController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperTradeShowNewTradeShowAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(TradeShowController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleAdminUserNewUserAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(UserController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperVendorNewVendorAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(VendorController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperVendorContactNewVendorContactAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(VendorContactController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperVendorContactTypeNewVendorContactTypeAction(ActionEvent event)
+//    {
+//        RootStageContainer.getCurrentRootStageContainer().changeView(VendorContactTypeController.VIEWCONTROLLER_TITLE);
+//    }
+//    @FXML
+//    protected void handleDeveloperCountryEditAction(ActionEvent event)
+//    {
+//
+//        openChildForm("NewCountryForm.fxml");
+//    }
 }
 
 
